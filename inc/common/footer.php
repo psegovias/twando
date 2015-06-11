1fecba60adfbc237d38b9bddc9224455551a017d
psegovias <patricio.segovia.s@gmail.com>
2015-06-11 17:45:41 -0300

    Update footer.php

--- inc/common/footer.php
+++ inc/common/footer.php
@@ -3,7 +3,6 @@
 </div>
 
 <div id="footer">
-  &copy;<?=date("Y")?> <a href="http://www.twando.com/" target="_blank">Twando.com</a> (Version <?=TWANDO_VERSION?>) | <a rel="nofollow" href="https://twitter.com/twando_com" target="_blank">Twando on Twitter</a>
 </div>
 
 <!-- End of container -->
