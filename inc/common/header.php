91cfcba67e3386fbc74e9ac08df85bb21cfbe0e0
psegovias <patricio.segovia.s@gmail.com>
2015-06-22 10:10:00 -0300

    Update header.php

--- inc/common/header.php
+++ inc/common/header.php
@@ -36,8 +36,9 @@ $(document).ready(function() {
 <div id="container">
 
 <!-- Header -->
+
 <div id="header_main">
-<a href="<?=BASE_LINK_URL?>"><h1>TWANDO</h1></a></div>
+  <a href="<?=BASE_LINK_URL?>"><img src="inc/images/logo.gif" width="407" height="65" alt="Twando" /></a>
 </div>
 
 <!-- Centre main -->
