5f99ae4cb8f6abceb5a372c7661a2dcec15617a3
psegovias <patricio.segovia.s@gmail.com>
2015-06-22 12:29:40 -0300

    Update not_logged_in.php

--- inc/content/english/not_logged_in.php
+++ inc/content/english/not_logged_in.php
@@ -33,13 +33,12 @@ if (!empty($_POST['password_login'])) {
 ?>
 <form method="post" action="<?=BASE_LINK_URL?>">
 Username:<br />
-<input type="text" name="username_login" size="20" class="input_box_style" value="<?php $username=?>" />
+<input type="text" name="username_login" size="20" class="input_box_style" value="<?=$_POST['username_login']?>" />
 <br />
 Password:<br />
-<input type="password" name="password_login" size="20" class="input_box_style" value="<?php $password=?>" />
+<input type="password" name="password_login" size="20" class="input_box_style" value="<?=$_POST['password_login']?>" />
 <br />
 <input type="submit" value="Sign In!" name="login" id="login" class="submit_button_style" />
 <input type="hidden" name="a" value="login2" />
-<input type="hidden" name="return_url" value="<?php $return_url?>" />
+<input type="hidden" name="return_url" value="<?=$return_url?>" />
 </form>
-
